a8a7b3ce16366117d981586f6a63e83798e9c3d3
Captcunnuk <lefebvrejames@rocketmail.com>
2020-04-18 00:46:06 -0400

    ArwhaalScene
    Egg throwing mechanic working with placeholder sprite (Kinda goes flying to infinity if spammed).

---
+++ Ludum46/Assets/Scripts/EggThrow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EggThrow : MonoBehaviour
+{
+    public Rigidbody2D rb_egg;
+    
+    bool Throw = false; 
+    public Vector2 eggThrow;
+
+    private void Start() 
+    {
+        //eggThrow.x = 250f;
+        //eggThrow.y = 250f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("mouse 0")){
+            Throw = true;
+        }
+    }
+
+    void FixedUpdate() 
+    {
+        if (Throw == true){
+            
+            rb_egg.AddForce(eggThrow);
+            Throw = false;
+        }
+    }
+
+}
