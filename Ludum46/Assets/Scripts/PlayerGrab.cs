e68a7ad78b3f7475fb9d5d106901d778c0ccd336
Captcunnuk <lefebvrejames@rocketmail.com>
2020-04-18 03:18:55 -0400

    Pushed Egg

---
+++ Ludum46/Assets/Scripts/PlayerGrab.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class PlayerGrab : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+
+    }
+        
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
