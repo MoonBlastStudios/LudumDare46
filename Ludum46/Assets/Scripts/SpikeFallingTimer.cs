162e7eb9ef0980a84125572c7d157c11037ab794
Emeraldblues <alebeau06@gmail.com>
2020-04-18 04:19:00 -0400

    added spike generator

---
+++ Ludum46/Assets/Scripts/SpikeFallingTimer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tools;
+using UnityEngine;
+
+public class SpikeFallingTimer : MonoBehaviour
+{
+    private Timer timer;
+    private float m_endTime = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = new Timer(m_endTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer.Tick(1))
+        {
+            Spawn();
+        }
+    }
+
+    public GameObject spike;
+    void Spawn()
+    {
+        Instantiate(spike);
+    }
+}
