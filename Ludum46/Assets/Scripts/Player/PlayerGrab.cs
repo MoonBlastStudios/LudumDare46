7f9a243b4014e5ef55f06a25fc57392470b2e45e
Captcunnuk <lefebvrejames@rocketmail.com>
2020-04-18 04:56:43 -0400

    Updated Throw code

--- Ludum46/Assets/Scripts/Player/PlayerGrab.cs
+++ Ludum46/Assets/Scripts/Player/PlayerGrab.cs
@@ -16,29 +16,35 @@ public class PlayerGrab : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(collision.gameObject.name);
-        if (collision.gameObject.name == "Egg"){
+        if (collision.gameObject.CompareTag("Egg"))
+        {
             touchingEgg = true;
-            collision.gameObject.layer = eggHoldingLayer;
         }
-        else {
-            touchingEgg = false;
-            collision.gameObject.layer = 0;
-        }
+    }
 
-        Debug.Log(touchingEgg);
+    private void OnCollisionExit2D(Collision2D collision) 
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Egg"))
+        {
+            touchingEgg = false;
+        }
     }
         
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("mouse 0") && touchingEgg == true){
+        if (Input.GetKeyDown("mouse 0") && touchingEgg == true)
+        {
             grabbedEgg = true;
             touchingEgg = false;
+            rbEgg.gameObject.layer = eggHoldingLayer;
         }
     }
 
     private void FixedUpdate() {
-        if (grabbedEgg == true){
+        if (grabbedEgg == true)
+        {
             rbEgg.MovePosition(rbPlayer.position + grabPosion * Time.deltaTime);
         }
     }
