0ead0dcb895facd39c1d5f005acb47fbd2c025a7
Emeraldblues <alebeau06@gmail.com>
2020-04-18 04:44:39 -0400

    Spike destroys on timer

---
+++ Ludum46/Assets/Scripts/Env/FallingSpikeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tools;
+using UnityEngine;
+
+public class FallingSpikeController : MonoBehaviour
+{
+    private Timer timer;
+    private float m_endTime = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = new Timer(m_endTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer.Tick(1))
+        {
+            Die();
+        }
+    }
+    
+    void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
