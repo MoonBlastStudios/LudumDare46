c2d7a94d5338d07f9268b582fc76f4f661944b70
Angelo Halabi <51285766+MoonBlastStudios@users.noreply.github.com>
2020-04-18 01:29:47 -0700

    Added Naming For Egg

---
+++ Ludum46/Assets/Scripts/Egg/EggNamer.cs
@@ -0,0 +1,28 @@
+﻿using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+
+namespace Egg
+{
+    public class EggNamer : MonoBehaviour
+    {
+        [SerializeField] private TextMeshPro m_eggNameLabel;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        [Button("Name The Egg")]
+        public void NameEgg(string p_name)
+        {
+            m_eggNameLabel.text = p_name;
+        }
+    }
+}
