61bd0c5e8b6c1f14003215183c3b7ddcf9bb6f8b
Angelo Halabi <51285766+MoonBlastStudios@users.noreply.github.com>
2020-04-19 15:44:19 -0700

    Updated Spike Extender and Level 1

--- Ludum46/Assets/Scripts/Env/SpikeExtender.cs
+++ Ludum46/Assets/Scripts/Env/SpikeExtender.cs
@@ -10,8 +10,8 @@ namespace Env
         [SerializeField] private float m_speed;
         [SerializeField] private float m_extendTimer;
         [SerializeField] private float m_extendMarginError = 0.01f;
-
-        private bool m_extend;
+        [SerializeField] private bool m_extend;
+        
         private Vector3 m_initialPosition;
         private Timer m_timer;
 
@@ -19,7 +19,9 @@ namespace Env
         void Start()
         {
             m_timer = new Timer(m_extendTimer);
-            m_initialPosition = transform.position;
+
+            m_initialPosition = m_extend ? transform.position - m_offset : transform.position;
+            
         }
 
         // Update is called once per frame
