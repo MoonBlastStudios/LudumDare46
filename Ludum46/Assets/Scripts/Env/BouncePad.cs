937f196870581d9c4d042b3078687d8c60763260
Emeraldblues <alebeau06@gmail.com>
2020-04-18 21:50:20 -0400

    bounce pad script and game over pop up script

---
+++ Ludum46/Assets/Scripts/Env/BouncePad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+    public int speed;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up*speed);
+        }
+    }
+}
---
+++ Ludum46/Assets/Scripts/Env/GameOverPopUpSpike.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverPopUpSpike : MonoBehaviour
+{
+    public GameObject gameOverMessage;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Spike")
+        {
+            Instantiate(gameOverMessage);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
