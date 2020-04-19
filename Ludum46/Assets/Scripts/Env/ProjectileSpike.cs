acb9c2ce3c90bc81dc95aa403689920e43d42b8f
Emeraldblues <alebeau06@gmail.com>
2020-04-19 00:56:02 -0400

    fixed spike spawner

---
+++ Ludum46/Assets/Scripts/Env/ProjectileSpike.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tools;
+using UnityEngine;
+
+public class ProjectileSpike : MonoBehaviour
+{
+    
+    private Rigidbody2D m_rb;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        m_rb.velocity = new Vector2(5,0);
+    }
+}
