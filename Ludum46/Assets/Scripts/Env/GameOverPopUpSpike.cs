fb6e9629baccb21e0087b02655c98fdacb0cb9c9
Angelo Halabi <51285766+MoonBlastStudios@users.noreply.github.com>
2020-04-19 23:58:45 -0700

    Added Respawn

--- Ludum46/Assets/Scripts/Env/GameOverPopUpSpike.cs
+++ Ludum46/Assets/Scripts/Env/GameOverPopUpSpike.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 
 public class GameOverPopUpSpike : MonoBehaviour
 {
-    public GameObject gameOverMessage;
+    private Vector3 m_spawnPoint;
+    
+    private void Awake()
+    {
+        m_spawnPoint = transform.position;
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Spike")
         {
-            Instantiate(gameOverMessage);
+            transform.position = m_spawnPoint;
         }
     }
 
